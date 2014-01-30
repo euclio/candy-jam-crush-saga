3de82b3b91a00704f10fa1fd1dd6fdfd47356435
smpsnr <smpsnr@gmail.com>
2014-01-29 16:11:05 -0800

    added HaxePunkTmx dependency and tilemap assets to project xml, display test map

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -1,7 +1,17 @@
 import com.haxepunk.Scene;
+import com.haxepunk.tmx.TmxEntity;
+import com.haxepunk.graphics.Image;
+
 
 class MainScene extends Scene {
     public override function begin() {
-        add(new Player(30, 50));
+        // create the map
+	var e = new TmxEntity("maps/test.tmx");
+	// load layers named bottom, main, top with the appropriate tileset
+	e.loadGraphic("graphics/tileset.png", ["ground", "collision"]);
+	// loads a grid layer named collision and sets the entity type to walls
+	//e.loadMask("collision", "walls");
+	add(e);
+	add(new Player(30, 50));
     }
 }

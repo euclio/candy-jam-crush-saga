21a2827a75778dfb2640663267dd43d3f40cc760
Andy Russell <arussell123@gmail.com>
2014-02-01 20:49:46 -0800

    refactor move distance into constant

--- src/Player.hx
+++ src/Player.hx
@@ -4,6 +4,8 @@ import com.haxepunk.Entity;
 import com.haxepunk.graphics.Image;
 
 class Player extends Entity {
+    private static inline var MOVE_DISTANCE:Int = 10;
+
     public function new(x: Int, y: Int) {
         super(x, y);
 
@@ -12,13 +14,13 @@ class Player extends Entity {
 
     public override function update() {
         if (Input.check(Key.LEFT) && collide("wall", x-10, y)==null) {
-            moveBy(-10, 0);
+            moveBy(-MOVE_DISTANCE, 0);
         } else if (Input.check(Key.RIGHT) && collide("wall", x+10, y)==null) {
-            moveBy(10, 0);
+            moveBy(MOVE_DISTANCE, 0);
         } else if (Input.check(Key.UP) && collide("wall", x, y-10)==null) {
-            moveBy(0, -10);
+            moveBy(0, -MOVE_DISTANCE);
         } else if (Input.check(Key.DOWN) && collide("wall", x, y+10)==null) {
-            moveBy(0, 10);
+            moveBy(0, MOVE_DISTANCE);
         }
     }
 }

8c7975636cc57dfb634b5f3b6142c8af91c1c2ed
Andy Russell <arussell123@gmail.com>
2014-02-02 15:46:17 -0800

    add Mission class

---
+++ src/Mission.hx
@@ -0,0 +1,9 @@
+class Mission {
+    public var text: String;
+    public var rules: List<String>;
+
+    public function new() {
+        text = "placeholder text";
+        rules = new List<String>();
+    }
+}

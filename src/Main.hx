893af3ce9401db9d11b49ac2a11a25bcd9d2e6af
smpsnr <smpsnr@gmail.com>
2014-02-02 14:20:21 -0800

    embed in test html file with swfobject, add larger test map

--- src/Main.hx
+++ src/Main.hx
@@ -1,13 +1,21 @@
 import com.haxepunk.Engine;
 import com.haxepunk.HXP;
 
-class Main extends Engine {
+class Main extends Engine 
+{
+    private static inline var framerate:Int = 30;
+    private static inline var screenWidth:Int = 640;
+    private static inline var screenHeight:Int =640;
+
     public override function init() {
-#if debug
-        HXP.console.enable();
-#end
         HXP.scene = new MainScene();
     }
 
-    public static function main() { new Main(); }
+    public function new() {
+        super(screenWidth, screenHeight, framerate, false);
+    }
+
+    static function main() {     
+        new Main(); 
+    }
 }

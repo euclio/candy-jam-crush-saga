dfe4967471a6af7aaae105a321227c972c223b61
Andy Russell <arussell123@gmail.com>
2014-02-02 17:33:15 -0800

    remove reference to Mission class

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -3,11 +3,7 @@ import com.haxepunk.graphics.Image;
 import com.haxepunk.tmx.TmxEntity;
 
 class MainScene extends Scene {
-    public var current_mission: Mission;
-
     public override function begin() {
-        current_mission = new Mission();
-
         var level = new TmxEntity("maps/test.tmx");
 
         // load the ground and collision layers, create a collision mask named

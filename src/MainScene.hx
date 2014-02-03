1b2a264be4247285a97ac01837f47655f8901eda
Andy Russell <arussell123@gmail.com>
2014-02-02 16:05:01 -0800

    retab

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -2,17 +2,22 @@ import com.haxepunk.Scene;
 import com.haxepunk.graphics.Image;
 import com.haxepunk.tmx.TmxEntity;
 
-class MainScene extends Scene 
-{
+class MainScene extends Scene {
+    public var current_mission: Mission;
+
     public override function begin() {
-	var level = new TmxEntity("maps/test.tmx");
-	
-	// load the ground and collision layers, create a collision mask named "wall" from the collision layer
-	level.loadGraphic("graphics/tileset.png", ["ground", "collision"]);
-	level.loadMask("collision", "wall");
-	
-	add(level);
-	
-	add(new Player(level.map.tileWidth, level.map.tileHeight));
+        current_mission = new Mission();
+
+        var level = new TmxEntity("maps/test.tmx");
+
+        // load the ground and collision layers, create a collision mask named
+        // "wall" from the collision layer
+        level.loadGraphic("graphics/tileset.png", ["ground", "collision"]);
+        level.loadMask("collision", "wall");
+
+        add(level);
+
+        add(new Player(level.map.tileWidth, level.map.tileHeight));
+        add(new Arcade(level.map.tileWidth * 5, level.map.tileHeight * 5, 10));
     }
 }

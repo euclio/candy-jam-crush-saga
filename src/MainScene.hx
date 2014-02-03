f8e04a113d0496968aa9168fe348e8fc0882930e
smpsnr <smpsnr@gmail.com>
2014-02-02 23:50:08 -0800

    implement loading arcade locations from tilemap

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -1,23 +1,25 @@
 import com.haxepunk.Scene;
 import com.haxepunk.graphics.Image;
 import com.haxepunk.tmx.TmxEntity;
+import com.haxepunk.tmx.TmxMap;
 
 class MainScene extends Scene {
     public override function begin() {
-        var level = new TmxEntity("maps/test.tmx");
+        var map = TmxMap.loadFromFile("maps/test.tmx");
+        var levelEntity = new TmxEntity(map);
 
         // load the ground and collision layers, create a collision mask named
         // "wall" from the collision layer
-        level.loadGraphic("graphics/tileset.png", ["ground", "collision"]);
-        level.loadMask("collision", "wall");
+        levelEntity.loadGraphic("graphics/tileset.png", ["ground", "collision"]);
+        levelEntity.loadMask("collision", "wall");
 
-        add(level);
+        add(levelEntity);
+        
+        for (object in levelEntity.map.getObjectGroup("arcade").objects) {
+            var arcadeType:ArcadeType = Type.createEnum(ArcadeType, object.type);
+            add(new Arcade(object.x, object.y, map.tileWidth, arcadeType));
+        }
 
-        add(new Player(level.map.tileWidth, level.map.tileHeight, level.map.tileWidth));
-        add(new Arcade(level.map.tileWidth * 5, level.map.tileHeight * 5, level.map.tileWidth, ArcadeType.apple));
-        add(new Arcade(level.map.tileWidth * 6, level.map.tileHeight * 5, level.map.tileWidth, ArcadeType.king));
-        add(new Arcade(level.map.tileWidth * 5, level.map.tileHeight * 6, level.map.tileWidth, ArcadeType.memory));
-        add(new Arcade(level.map.tileWidth * 7, level.map.tileHeight * 5, level.map.tileWidth, ArcadeType.edge));
-        add(new Arcade(level.map.tileWidth * 5, level.map.tileHeight * 7, level.map.tileWidth, ArcadeType.scroll));
+        add(new Player(map.tileWidth, map.tileHeight, map.tileWidth));
     }
 }
